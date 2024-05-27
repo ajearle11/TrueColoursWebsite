4674b5b4c551cf4a0f323d15ae2cac9d5f0b4cd2
ajearle11 <58401691+ajearle11@users.noreply.github.com>
2024-05-27 17:19:15 +0100

    Added working form

--- true-colours/src/components/form.tsx
+++ true-colours/src/components/form.tsx
@@ -4,6 +4,7 @@ import { Button } from "./";
 import { styles } from "../styles";
 import Lottie from "lottie-react";
 import sentAnimation from "../assets/Sent.json";
+import emailjs from "@emailjs/browser";
 
 const FormComponent = () => {
   const [name, setName] = useState<string>("");
@@ -14,6 +15,9 @@ const FormComponent = () => {
   const [contactError, setContactError] = useState<boolean>(false);
   const [messageError, setMessageError] = useState<boolean>(false);
   const [successSent, setSuccessSent] = useState<boolean>(false);
+  const [deodoriserBool, setDeodoriserBool] = useState<boolean>(false);
+  const [stainGuardBool, setStainGuardBool] = useState<boolean>(false);
+  const [messageSendError, setMessageSendError] = useState<boolean>(false);
 
   //   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
@@ -30,18 +34,37 @@ const FormComponent = () => {
     }
 
     if (name && contactDetails && message) {
-      const data = {
-        name,
-        contactDetails,
-        emailDetails,
-        message,
-      };
-      console.log(data);
-      setSuccessSent(true);
-      setName("");
-      setContactDetails("");
-      setEmailDetails("");
-      setMessage("");
+      const templateParams = `Name: ${name}\nContact Details: ${contactDetails}\nEmail: ${emailDetails}\nMessage: ${message}\nDeodoriser: ${deodoriserBool}\nStain Guard: ${stainGuardBool}`;
+
+      emailjs
+        .send(
+          import.meta.env.VITE_YOUR_SERVICE_ID,
+          import.meta.env.VITE_YOUR_TEMPLATE_ID,
+          {
+            to_name: "Alex",
+            from_name: "EmailJS",
+            message: templateParams,
+          },
+          {
+            publicKey: import.meta.env.VITE_YOUR_PUBLIC_KEY,
+          }
+        )
+        .then(
+          () => {
+            console.log("SUCCESS!");
+            setSuccessSent(true);
+            setName("");
+            setContactDetails("");
+            setEmailDetails("");
+            setMessage("");
+            setStainGuardBool(false);
+            setDeodoriserBool(false);
+          },
+          (error) => {
+            setMessageSendError(true);
+            console.log("FAILED...", error.text);
+          }
+        );
     }
   };
 
@@ -130,15 +153,15 @@ const FormComponent = () => {
               type="text"
               value={emailDetails}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                
                 setEmailDetails(e.target.value);
               }}
             />
-            
           </div>
 
           <div className="form-group">
-            <label style={{textAlign: 'start'}}>Tell us what you'd like cleaning</label>
+            <label style={{ textAlign: "start" }}>
+              Tell us what you'd like cleaning
+            </label>
             <TextareaAutosize
               minRows={4}
               style={{
@@ -164,21 +187,69 @@ const FormComponent = () => {
                 You must enter a message
               </p>
             ) : null}
-                <p
-                    style={{
-                      fontSize: styles.fontSizes.xSmall,
-                      fontWeight: "bold",
-                      fontStyle: 'italic',
-                    }}
-                  >
-                    *If applicable, please provide approximate dimensions for rooms
-                  </p>
+            <p
+              style={{
+                fontSize: styles.fontSizes.xSmall,
+                fontWeight: "bold",
+                fontStyle: "italic",
+              }}
+            >
+              *If applicable, please provide approximate dimensions for rooms
+            </p>
+            <label
+              style={{
+                marginTop: "30px",
+                marginBottom: "10px",
+                fontSize: styles.fontSizes.small,
+                fontWeight: "bold",
+                textAlign: "start",
+              }}
+            >
+              Please tick if you're interested in the following:
+            </label>
+            <div style={{ display: "flex", marginBottom: "10px" }}>
+              <input
+                type="checkbox"
+                id="deodoriser"
+                name="deodoriser"
+                style={{ marginRight: "10px" }}
+                onChange={() => {
+                  setDeodoriserBool(!deodoriserBool);
+                }}
+                checked={deodoriserBool}
+              />
+              <label htmlFor="deodoriser">Deodoriser Treatment</label>
+            </div>
+            <div style={{ display: "flex" }}>
+              <input
+                type="checkbox"
+                style={{ marginRight: "10px" }}
+                id="stain-guard"
+                name="stain-guard"
+                onChange={() => {
+                  setStainGuardBool(!stainGuardBool);
+                }}
+                checked={stainGuardBool}
+              />
+              <label htmlFor="stain-guard">Stain Guard Treatment</label>
+            </div>
           </div>
+          {messageSendError ? (
+            <p
+              style={{
+                fontSize: styles.fontSizes.xSmall,
+                color: styles.colors.red,
+                fontWeight: "bold",
+              }}
+            >
+              Error sending a message. Try again.
+            </p>
+          ) : null}
         </form>
       )}
 
       {!successSent ? (
-        <Button text="Submit" onClick={handleSubmit} />
+        <Button text="Get My Quote" onClick={handleSubmit} />
       ) : (
         <Button text="New Form" onClick={() => setSuccessSent(false)} />
       )}
